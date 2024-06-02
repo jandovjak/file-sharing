b1cdd926b6db2c7d73185238e130a2c25af296b9
Ján Dovjak <dovjak.jan@gmail.com>
2024-06-02 15:17:58 +0200

    encryption/decryption updated

--- app.py
+++ app.py
@@ -1,46 +1,61 @@
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA3_256
 from Crypto.Util.Padding import pad, unpad
-
+import secrets
 
 BLOCK_SIZE = 16
 
 
-def hash_password(password: str) -> bytearray:
+def generate_iv() -> bytes:
+    return secrets.token_bytes(BLOCK_SIZE)
+
+
+def hash_password(password: str) -> bytes:
     hash_object = SHA3_256.new()
     password_bytes = bytearray(password.encode('utf-8'))
     hash_object.update(password_bytes)
-    return bytearray(hash_object.digest())
+    return bytes(hash_object.digest())
 
 
-def encrypt(plaintext, key, iv):
+def encrypt_with_iv(plaintext: bytes, key: bytes, iv: bytes) -> bytes:
     cipher = AES.new(key, AES.MODE_CBC, iv)
     return cipher.encrypt(pad(plaintext, BLOCK_SIZE))
 
 
-def decrypt(ciphertext, key, iv):
+def encrypt(plaintext: bytes, key: bytes) -> bytes:
+    iv = generate_iv()
+    return iv + encrypt_with_iv(plaintext, key, iv)
+
+
+def decrypt_with_iv(ciphertext: bytes, key: bytes, iv: bytes) -> bytes:
     cipher = AES.new(key, AES.MODE_CBC, iv)
     return unpad(cipher.decrypt(ciphertext), BLOCK_SIZE)
 
 
-def encrypt_file(input_file, output_file, key, iv) -> None:
+def decrypt(ciphertext: bytes, key: bytes) -> bytes:
+    iv = ciphertext[:BLOCK_SIZE]
+    return decrypt_with_iv(ciphertext[BLOCK_SIZE:], key, iv)
+
+
+def encrypt_file(input_file: str, output_file: str, key: bytes) -> None:
     with open(input_file, 'rb') as f:
         plaintext = f.read()
-    ciphertext = encrypt(plaintext, key, iv)
+    ciphertext = encrypt(plaintext, key)
     with open(output_file, 'wb') as f:
         f.write(ciphertext)
 
 
-def decrypt_file(input_file, output_file, key, iv) -> None:
+def decrypt_file(input_file: str, output_file: str, key: bytes) -> None:
     with open(input_file, 'rb') as f:
         ciphertext = f.read()
-    plaintext = decrypt(ciphertext, key, iv)
+    plaintext = decrypt(ciphertext, key)
     with open(output_file, 'wb') as f:
         f.write(plaintext)
 
 
 if __name__ == '__main__':
-    key = hash_password('aaaa')
-    iv = bytes([0] * 16)
-    encrypt_file('files/example.txt', 'files/example.bin', key, iv)
-    decrypt_file('files/example.bin', 'files/example2.txt', key, iv)
+    key = hash_password('MySuperSecretPassword123!')
+    encrypt_file('files/example.txt', 'files/example.bin', key)
+    decrypt_file('files/example.bin', 'files/example2.txt', key)
+    decrypt_file('files/5cd994a7-c9a6-442a-a3b6-be1e2a9955ef',
+                 'files/example_decrypted.txt', key)
