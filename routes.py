9689cd8f6661cad149d994198ca5c266dd21a607
Ján Dovjak <dovjak.jan@gmail.com>
2024-06-02 16:34:13 +0200

    Validation checking for GUID and file operatons

--- routes.py
+++ routes.py
@@ -1,66 +1,106 @@
 from fastapi import APIRouter, Depends, HTTPException, UploadFile, File, Form
-from fastapi.responses import StreamingResponse
+from fastapi.responses import Response, StreamingResponse
 from sqlalchemy.orm import Session
 from uuid import UUID
 from io import BytesIO
+from database import get_db
+from config import FILE_LOCATION
+from app import decrypt, encrypt, hash_password
+
 import crud
 import schemas
 import os
-from database import get_db
-from config import FILE_LOCATION
-from app import encrypt, hash_password
 
 router = APIRouter()
+
+
+def is_valid_uuid(uuid: str) -> bool:
+    try:
+        uuid_object = UUID(uuid)
+    except ValueError:
+        return False
+    return str(uuid_object) == uuid
 
 
 @router.post("/upload", response_model=schemas.File)
 async def upload_file(file: UploadFile = File(...),
                       password: str = Form(...),
                       db: Session = Depends(get_db)):
-    file_size = file.size
     file_data = schemas.FileCreate(filename=file.filename,
-                                   size=file_size)
+                                   size=file.size)
     created_file = crud.create_file(db=db, file=file_data)
     file_location = f"{FILE_LOCATION}/{created_file.id}"
-    contents = await file.read()
+    file_content = await file.read()
     key = hash_password(password)
-    encrypted = encrypt(contents, key)
-    with open(file_location, "wb+") as file_object:
-        file_object.write(encrypted)
-    return created_file
+    encrypted = encrypt(file_content, key)
+    try:
+        with open(file_location, "wb") as file_object:
+            file_object.write(encrypted)
+            return created_file
+    except FileNotFoundError:
+        raise HTTPException(status_code=404,
+                            detail="File could not be created")
+    except PermissionError:
+        raise HTTPException(status_code=404,
+                            detail="File could not be created")
+    except Exception:
+        raise HTTPException(status_code=500,
+                            detail="Unexpected error happened")
 
 
-@router.get("/files/{file_id}", response_model=schemas.File)
-def read_file(file_id: UUID,
+@router.post("/download")
+def read_file(id: str = Form(...),
               password: str = Form(...),
+              decrypt_file: bool = Form(None),
               db: Session = Depends(get_db)):
-    file = crud.get_file(db, file_id=file_id)
-    if file is None:
-        raise HTTPException(status_code=404, detail="File not found")
-    file_location = f"{FILE_LOCATION}/{file.id}"
-    with open(file_location, "rb+") as file_object:
-        data = file_object.read()
-        headers = {
-            'Content-Disposition': f"attachment; filename={file.filename}"
-        }
-        return StreamingResponse(BytesIO(data),
-                                 headers=headers,
-                                 media_type='application/octet-stream')
-
-
-@router.delete("/files/{file_id}", response_model=schemas.File)
-def delete_file(file_id: UUID, db: Session = Depends(get_db)):
-    file = crud.delete_file(db, file_id=file_id)
+    if not is_valid_uuid(id):
+        raise HTTPException(status_code=404, detail="Invalid ID")
+    file = crud.get_file(db, id=id)
     if file is None:
         raise HTTPException(status_code=404, detail="File not found")
     file_location = f"{FILE_LOCATION}/{file.id}"
     try:
-        os.remove(file_location)
-        print(f"The file {file_location} has been removed successfully.")
+        with open(file_location, "rb") as file_object:
+            key = hash_password(password)
+            data = file_object.read()
+            if decrypt_file is not None and decrypt_file:
+                data = decrypt(data, key)
+            headers = {
+                'Content-Disposition': f"attachment; filename={file.filename}"
+            }
+            return StreamingResponse(BytesIO(data),
+                                     headers=headers,
+                                     media_type='application/octet-stream')
     except FileNotFoundError:
-        print(f"The file {file_location} does not exist.")
+        raise HTTPException(status_code=404,
+                            detail="File does not exist")
     except PermissionError:
-        print(f"Permission denied: unable to delete {file_location}.")
-    except Exception as e:
-        print(f"Error occurred: {e}")
-    return file
+        raise HTTPException(status_code=404,
+                            detail="Unable to delete file")
+    except Exception:
+        raise HTTPException(status_code=500,
+                            detail="Unexpected error happened")
+
+
+@router.post("/delete", response_model=schemas.File)
+def delete_file(id: str = Form(...),
+                db: Session = Depends(get_db)):
+    if not is_valid_uuid(id):
+        raise HTTPException(status_code=404, detail="Invalid ID")
+    file = crud.delete_file(db, id=id)
+    if file is None:
+        raise HTTPException(status_code=404,
+                            detail="File not found")
+    file_location = f"{FILE_LOCATION}/{file.id}"
+    try:
+        os.remove(file_location)
+        return Response(status_code=204)
+    except FileNotFoundError:
+        raise HTTPException(status_code=404,
+                            detail="File does not exist")
+    except PermissionError:
+        raise HTTPException(status_code=404,
+                            detail="Unable to delete file")
+    except Exception:
+        raise HTTPException(status_code=500,
+                            detail="Unexpected error happened")
