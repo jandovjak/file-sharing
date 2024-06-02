ba45ba7f6390166464fab8f35cd72111700a7950
Ján Dovjak <dovjak.jan@gmail.com>
2024-06-02 15:18:25 +0200

    Upload and download need password

--- routes.py
+++ routes.py
@@ -1,38 +1,51 @@
-from fastapi import APIRouter, Depends, HTTPException, UploadFile, File
-from fastapi.responses import FileResponse
+from fastapi import APIRouter, Depends, HTTPException, UploadFile, File, Form
+from fastapi.responses import StreamingResponse
 from sqlalchemy.orm import Session
-import shutil
 from uuid import UUID
+from io import BytesIO
 import crud
 import schemas
 import os
 from database import get_db
 from config import FILE_LOCATION
+from app import encrypt, hash_password
 
 router = APIRouter()
 
 
 @router.post("/upload", response_model=schemas.File)
-def upload_file(file: UploadFile = File(...),
-                db: Session = Depends(get_db)):
+async def upload_file(file: UploadFile = File(...),
+                      password: str = Form(...),
+                      db: Session = Depends(get_db)):
     file_size = file.size
     file_data = schemas.FileCreate(filename=file.filename,
                                    size=file_size)
     created_file = crud.create_file(db=db, file=file_data)
     file_location = f"{FILE_LOCATION}/{created_file.id}"
+    contents = await file.read()
+    key = hash_password(password)
+    encrypted = encrypt(contents, key)
     with open(file_location, "wb+") as file_object:
-        shutil.copyfileobj(file.file, file_object)
+        file_object.write(encrypted)
     return created_file
 
 
 @router.get("/files/{file_id}", response_model=schemas.File)
-def read_file(file_id: UUID, db: Session = Depends(get_db)):
+def read_file(file_id: UUID,
+              password: str = Form(...),
+              db: Session = Depends(get_db)):
     file = crud.get_file(db, file_id=file_id)
     if file is None:
         raise HTTPException(status_code=404, detail="File not found")
-    return FileResponse(f"{FILE_LOCATION}/{file.id}",
-                        media_type='application/octet-stream',
-                        filename=file.filename)
+    file_location = f"{FILE_LOCATION}/{file.id}"
+    with open(file_location, "rb+") as file_object:
+        data = file_object.read()
+        headers = {
+            'Content-Disposition': f"attachment; filename={file.filename}"
+        }
+        return StreamingResponse(BytesIO(data),
+                                 headers=headers,
+                                 media_type='application/octet-stream')
 
 
 @router.delete("/files/{file_id}", response_model=schemas.File)
